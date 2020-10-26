25f7ee338619d0cf8636a1855478b227326bf2ca
yhkdev <email@email.com>
2020-10-26 14:43:10 -0400

    Remove argparse

--- colorization_algorithms/demo_release.py
+++ colorization_algorithms/demo_release.py
@@ -1,5 +1,4 @@
-
-import argparse
+import sys
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -23,25 +22,20 @@ RESULT_IMG_SAVE_PATH = "static/img/results_img/"
 
 
 def main(filepath, model_dir):
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-i','--img_path', type=str, default=filepath)  # default='imgs/ansel_adams3.jpg'
-	parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
-	parser.add_argument('-o','--save_prefix', type=str, default='saved', help='will save into this file with {eccv16.png, siggraph17.png} suffixes')
-	opt = parser.parse_args()
 
 	# load colorizers
 	colorizer_eccv16 = eccv16(model_dir, pretrained=True).eval()
 	colorizer_siggraph17 = siggraph17(model_dir, pretrained=True).eval()
-	if(opt.use_gpu):
-		colorizer_eccv16.cuda()
-		colorizer_siggraph17.cuda()
+	# if(opt.use_gpu):
+	# 	colorizer_eccv16.cuda()
+	# 	colorizer_siggraph17.cuda()
 
 	# default size to process images is 256x256
 	# grab L channel in both original ("orig") and resized ("rs") resolutions
-	img = load_img(opt.img_path)
+	img = load_img(filepath)
 	(tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
-	if(opt.use_gpu):
-		tens_l_rs = tens_l_rs.cuda()
+	# if(opt.use_gpu):
+	# 	tens_l_rs = tens_l_rs.cuda()
 
 	# colorizer outputs 256x256 ab map
 	# resize and concatenate to original L channel
@@ -51,8 +45,8 @@ def main(filepath, model_dir):
 
 	# Save Resulting Images
 	save_path = RESULT_IMG_SAVE_PATH
-	plt.imsave(f'{save_path}%s_eccv16.png'%opt.save_prefix, out_img_eccv16)
-	plt.imsave(f'{save_path}%s_siggraph17.png'%opt.save_prefix, out_img_siggraph17)
+	plt.imsave(f'{save_path}%s_eccv16.png'%"saved", out_img_eccv16)
+	plt.imsave(f'{save_path}%s_siggraph17.png'%"saved", out_img_siggraph17)
 
 	plt.figure(figsize=(12,8))
 	plt.subplot(2,2,1)
